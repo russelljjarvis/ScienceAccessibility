ace2b06f478bba6ac6c63a1d7e8716f179f97f7f
Russell Jarvis <rjjarvis@asu.edu>
2018-05-21 18:33:04 -0700

    massive clean up of stagnant cruft

--- read_mat.m
+++
@@ -1,4 +0,0 @@
-data_GMO = load("GMO/GMO0.mat")
-data_Genetically_Modified_Organism = load("Genetically_Modified_Organism/Genetically_Modified_Organism0.mat")
-data_Transgenic = load("Transgenic/Transgenic0.mat")
-

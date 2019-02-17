016e6cce5036584d48b66869c87fdba1790a373b
Russell Jarvis <colouredstatic@gmail.com>
2019-02-17 13:43:02 -0700

    Update compete.py
    # Put these results, in a data frame, then in Markdown, using RGerkin's code.

--- Examples/compete.py
+++ Examples/compete.py
@@ -121,6 +121,32 @@ rank = [(rick,str('rick')),(scrook,str('sharon')),(grayden,str('grayden'))]
 print('the winner of the science clarity competition is: ', sorted(rank)[0])
 print(rank)
 print(rick,scrook,grayden)
+
+# Put these results, in a data frame, then in Markdown, using RGerkin's code.
+# https://gist.github.com/rgerkin/af5b27a0e30531c30f2bf628aa41a553
+# !pip install --user tabulate # Install the tabulate package
+from tabulate import tabulate
+import numpy as np
+import pandas as pd
+import IPython.display as d
+# Some random data
+data = np.random.rand(10,4)
+# Columns A, B, C, D
+columns = [chr(x) for x in range(65,69)]
+# Create the dataframe
+df = pd.DataFrame(data=data, 
+columns=columns)
+# Optionally give the dataframe's index a name
+#df.index.name = "my_index"
+# Create the markdown string
+md = tabulate(df, headers='keys', tablefmt='pipe')
+# Fix the markdown string; it will not render with an empty first table cell, 
+# so if the dataframe's index has no name, just place an 'x' there.  
+md = md.replace('|    |','| %s |' % (df.index.name if df.index.name else 'x'))
+# Create the Markdown object
+result = d.Markdown(md)
+# Display the markdown object (in a Jupyter code cell)
+result
 #print('the scores are:',np.min(rick,sharon))
 '''
 bench = metrics(bench)

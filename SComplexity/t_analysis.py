9cd8acd30c4dcdbdad8aa6c026d6f67e419363c3
Russell Jarvis <colouredstatic@gmail.com>
2019-02-25 12:09:01 -0700

    Update t_analysis.py

--- SComplexity/t_analysis.py
+++ SComplexity/t_analysis.py
@@ -35,45 +35,6 @@ from SComplexity.utils import (black_string, clue_links, clue_words,
 from tabulate import tabulate
 from textblob import TextBlob
 from textstat.textstat import textstat
-
-# setting of an appropriate backend not an X11 one.
-
-
-
-#text analysis imports
-
-def isPassive(sentence):
-    # https://github.com/flycrane01/nltk-passive-voice-detector-for-English/blob/master/Passive-voice.py
-    beforms = ['am', 'is', 'are', 'been', 'was', 'were', 'be', 'being']               # all forms of "be"
-    aux = ['do', 'did', 'does', 'have', 'has', 'had']                                  # NLTK tags "do" and "have" as verbs, which can be misleading in the following section.
-    words = nltk.word_tokenize(sentence)
-    tokens = nltk.pos_tag(words)
-    tags = [i[1] for i in tokens]
-    if tags.count('VBN') == 0:                                                            # no PP, no passive voice.
-        return False
-    elif tags.count('VBN') == 1 and 'been' in words:                                    # one PP "been", still no passive voice.
-        return False
-    else:
-        pos = [i for i in range(len(tags)) if tags[i] == 'VBN' and words[i] != 'been']  # gather all the PPs that are not "been".
-        for end in pos:
-            chunk = tags[:end]
-            start = 0
-            for i in range(len(chunk), 0, -1):
-                last = chunk.pop()
-                if last == 'NN' or last == 'PRP':
-                    start = i                                                             # get the chunk between PP and the previous NN or PRP (which in most cases are subjects)
-                    break
-            sentchunk = words[start:end]
-            tagschunk = tags[start:end]
-            verbspos = [i for i in range(len(tagschunk)) if tagschunk[i].startswith('V')] # get all the verbs in between
-            if verbspos != []:                                                            # if there are no verbs in between, it's not passive
-                for i in verbspos:
-                    if sentchunk[i].lower() not in beforms and sentchunk[i].lower() not in aux:  # check if they are all forms of "be" or auxiliaries such as "do" or "have".
-                        break
-                else:
-                    return True
-    return False
-
 tagger = PerceptronTagger(load=False)
 
 def unigram(tokens):
